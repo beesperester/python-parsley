d0fe0cefa41edca13ee25b5ae21ac04df92c20e3
Bernhard Esperester <bernhard@esperester.de>
2020-10-27 14:07:25 +0100

    adds license and setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="parsley-beesperester",
+    version="0.0.1",
+    author="Bernhard Esperester",
+    author_email="bernhard@esperester.de",
+    description="Use simple recipes to extract, mutate and validate data from BeautifulSoup objects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/beesperester/python-parsley",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
